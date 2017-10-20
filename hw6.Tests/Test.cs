6898dc9197d7a61e05b057e8827d72e6cf6825bf
Jakob <jadppf@mail.missouri.edu>
2017-10-20 15:13:59 -0500

    maybe this works

---
+++ hw6.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace hw6.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
